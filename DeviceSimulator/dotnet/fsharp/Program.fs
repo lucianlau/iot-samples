a9e70838403c8731028669b0206f86163a15adaa
William Berry <wberry@microsoft.com>
2016-09-08 12:14:09 -0700

    Add CSharp Device Simulator and Cloud Reader projects Wire-up FSharp solution and projects from exiting work

---
+++ DeviceSimulator/dotnet/fsharp/Program.fs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+// Learn more about F# at http://fsharp.org
+
+namespace Azure.IoTHub.Examples.FSharp.SimulatedDevice
+
+open System 
+open System.Text
+open System.Device.Location
+open System.Threading
+open FSharp.Configuration
+open Microsoft.Azure.Devices.Client
+open Newtonsoft.Json
+
+type Config = YamlConfig<FilePath="config.yaml", ReadOnly=true>
+
+type telemetryDataPoint = {
+    location : GeoCoordinate 
+    deviceId : string 
+    windSpeed : float 
+    }
+
+[<Measure>]
+type meters 
+
+module Main = 
+    
+    [<EntryPoint>]
+    let main argv = 
+        let config = Config()
+        let iotHubUri   = config.AzureIoTHub.IoTHubUri
+        let deviceKey   = config.AzureIoTHub.DeviceKey
+        let deviceId    = config.AzureIoTHub.DeviceId
+    
+        let deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey))
+    
+        let avgWindSpeed = 10.
+        let rand = new Random()
+    
+        let windSpeedMessage location = Seq.initInfinite (fun index -> 
+                let telemetryReading = { 
+                    deviceId = deviceId
+                    windSpeed = (avgWindSpeed + rand.NextDouble() * 4. - 2.) 
+                    location = location
+                    }
+                let json = JsonConvert.SerializeObject(telemetryReading)
+                index, new Message(Encoding.ASCII.GetBytes(json)), json
+            )
+
+        let getRandomGeoCoordinate seed (lat : float) (long : float) (radius : float) : GeoCoordinate = 
+            // check out http://gis.stackexchange.com/a/68275 for where this calc originated.
+            let rand = new Random(seed)
+            let u = rand.NextDouble()
+            let v = rand.NextDouble()
+            let w = radius / 111000. * Math.Sqrt(u)
+            let t = 2. * Math.PI * v
+            let x = (w * Math.Cos(t)) / Math.Cos(lat)
+            let y = w * Math.Sin(t)
+            GeoCoordinate(y+lat, x+long)          
+    
+        let dataSendTask location = 
+            async {
+                windSpeedMessage location |> Seq.iter (fun (index, message, json) -> 
+                    deviceClient.SendEventAsync(message) |> Async.AwaitIAsyncResult |> Async.Ignore |> ignore
+                    printfn "%O > Sending message %i: %s" (DateTime.Now.ToString()) index json
+                    Thread.Sleep 10000
+                    )
+            } 
+        
+        let nycSites = Array.init 10 (fun index -> getRandomGeoCoordinate index 40.7128 74.0059 (20000.))
+        
+        nycSites 
+        |> Array.map dataSendTask
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore
+    
+        printfn "%A" argv
+        0 // return an integer exit code
+    
+
+//*********************************************************
+//
+//Azure.IotHub.Examples.FSharp, https://github.com/WilliamBerryiii/Azure.IotHub.Examples.FSharp
+//
+//Copyright (c) Microsoft Corporation
+//All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+
+
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+
+
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//*********************************************************
---
+++ ReadDeviceToCloudMessages/dotnet/fsharp/Program.fs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+// Learn more about F# at http://fsharp.org
+
+namespace Azure.IoTHub.Examples.FSharp.ReadDeviceToCloudMessages
+
+open System
+open System.Text
+open System.Threading
+open System.Threading.Tasks
+open FSharp.Configuration
+open Microsoft.ServiceBus.Messaging 
+
+type Config = YamlConfig<FilePath="config.yaml", ReadOnly=true>
+
+module Main = 
+    
+    [<EntryPoint>]
+    let main argv = 
+        let config = Config()
+        let connectionString = config.AzureIoTHub.ConnectionString
+        let iotHubD2cEndpoint = config.AzureIoTHub.EndPoint
+        
+        let eventHubClient connectionString iotHubD2cEndpoint = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint)
+        let eventHubReceiver (eventHubClient : EventHubClient) (partition : string) = 
+            eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow)
+         
+        let receiveMessagesFromDeviceAsync (eventHubReceiver:EventHubReceiver) = 
+            async {
+                while true do
+                    let! result = eventHubReceiver.ReceiveAsync() |> Async.AwaitTask
+                    match result with 
+                    | null -> ()
+                    | _ -> 
+                        let message = Encoding.UTF8.GetString(result.GetBytes())
+                        printfn "Message received. Partition: %A Data: '%A'" eventHubReceiver.PartitionId message
+            }
+    
+        printfn "Receive messages. Ctrl-C to exit.\n"
+        
+        let hubClient = eventHubClient connectionString iotHubD2cEndpoint
+    
+        let runtimeData = hubClient.GetRuntimeInformation()
+        let d2cPartitions = runtimeData.PartitionIds
+            
+        let cts = new CancellationTokenSource()
+    
+        let tasks = d2cPartitions 
+                    |> Array.map (fun partition -> 
+                        partition
+                        |> eventHubReceiver hubClient
+                        |> receiveMessagesFromDeviceAsync
+                        )
+        
+        Async.Start(tasks |> Async.Parallel |> Async.Ignore, cts.Token)
+    
+        System.Console.CancelKeyPress.Add(fun (arg) -> 
+            arg.Cancel <- true
+            cts.Cancel()
+            printfn "Exiting...")
+    
+        Console.ReadLine() |> ignore
+    
+        printfn "%A" argv
+        0 // return an integer exit code
+
+
+//*********************************************************
+//
+//Azure.IotHub.Examples.FSharp, https://github.com/WilliamBerryiii/Azure.IotHub.Examples.FSharp
+//
+//Copyright (c) Microsoft Corporation
+//All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+
+
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+
+
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//*********************************************************
