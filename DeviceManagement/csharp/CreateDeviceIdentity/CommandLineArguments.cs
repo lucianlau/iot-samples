9e836b12bfc11979a63fa8e6581c82ff2a7092b0
Dag König <dag@konig.nu>
2017-03-22 14:46:30 +0100

    added arguments to create device project to automate mass creation of devices

---
+++ DeviceManagement/csharp/CreateDeviceIdentity/CommandLineArguments.cs
@@ -0,0 +1,19 @@
+﻿using CommandLineParser.Arguments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.IoTHub.Examples.CSharp.CreateDeviceIdentity
+{
+    class CommandLineArguments
+    {
+        [ValueArgument(typeof(int), 'n', "number-of-devices", Description = "Number of devices to create", DefaultValue = -1)]
+        public int NumberOfDeviceToCreate;
+
+        [ValueArgument(typeof(string), 'p', "device-name-prefix", Description = "Prefix of device name", DefaultValue = "dev")]
+        public string DeviceNamePrefix;
+
+    }
+}
